2df6c7119fd5c961d405ebf3fbae04bae0fd2342
Ilyaas Baker <91404740+ilyaasbaker@users.noreply.github.com>
2024-04-08 01:27:09 +0100

    ll

---
+++ Social Media Concept and Frontend Design/Story Dice App/models/story-data.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+let story = {
+
+    // ID's and Keys
+    storyID: '', // string?
+    snippetGroup: snippetID, 
+
+    // Name 
+    storyName: '', // title of the story
+
+    // Story Contents 
+    compiledStory: [
+        
+        {userResponse, snippetID, userID, timePosted, snippetNumber}, 
+        {userResponse, snippetID, userID, timePosted, snippetNumber} 
+    
+    ], // an array containing the user responses and some extra info to differentiate each users snippet
+
+
+    // breathing space
+}
